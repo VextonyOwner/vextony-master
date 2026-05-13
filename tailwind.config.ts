6202ac1a1f41e499b12f61a7fdb404f8caf5a087
MindSpark Universe <259244303+VextonyOwner@users.noreply.github.com>
2026-05-13 16:02:47 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        gold: "#D4AF37",
+        gold: '#D4AF37',
       },
     },
   },
   plugins: [],
 };
+
 export default config;

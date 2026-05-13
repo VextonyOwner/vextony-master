d9e7f868657f8077582fed15bb8b0dcd92b4da99
MindSpark Universe <259244303+VextonyOwner@users.noreply.github.com>
2026-05-13 17:44:10 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  poweredByHeader: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/vault/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'self' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'DENY' }
-        ],
-      },
-    ];
-  }
 };
 
 export default nextConfig;

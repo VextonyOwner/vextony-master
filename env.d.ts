4706b319cf5bd61050fa85dcdc775c8a83bb5f5c
MindSpark Universe <259244303+VextonyOwner@users.noreply.github.com>
2026-05-13 16:17:37 +0600

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,1 @@
-// env.d.ts
-namespace NodeJS {
-  interface ProcessEnv {
-    MONGODB_URI: string;
-    GEMINI_API_KEY: string;
-    GROQ_API_KEY: string;
-    NODE_ENV: 'development' | 'production';
-  }
-}
+

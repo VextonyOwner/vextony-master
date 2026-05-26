d8eb12fc034685519ad52351f2edd3a6cb92a23f
MindSpark Universe <259244303+VextonyOwner@users.noreply.github.com>
2026-05-26 22:20:16 +0600

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/**
+ * VEXTONY CORE INFRASTRUCTURE: POSTCSS CSS ENGINE COMPLIANCE SHIELD
+ * [STATUS: OMNIPOTENT ULTRA-MAX PRODUCTION LIVE] | [PRIVILEGE: COMPILER INGRESS]
+ */
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};

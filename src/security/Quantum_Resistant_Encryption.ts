c2ad19603f46aa1cd0b78261c3c985c5122957bd
MindSpark Universe <259244303+VextonyOwner@users.noreply.github.com>
2026-07-20 17:44:49 +0600

    Update Quantum_Resistant_Encryption.ts

--- src/security/Quantum_Resistant_Encryption.ts
+++ src/security/Quantum_Resistant_Encryption.ts
@@ -16,7 +16,7 @@ export class QuantumResistantEncryption {
   private static encryptionInstance: QuantumResistantEncryption;
   private readonly encryptionLinearKey: string = "vxt_quantum_linear_asif_encryption_2026";
   private isCryptoModuleOnline: boolean = true;
-  private readonly saltVectorArray: number[] =; // Sovereign key expansion primitives
+  private readonly saltVectorArray: any =; // Sovereign key expansion primitives
 
   private constructor() {}
 

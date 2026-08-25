63d6dba11d874cbee4a87ee94e5fb80dc4ff364a
MindSpark Universe <259244303+VextonyOwner@users.noreply.github.com>
2026-08-25 15:12:04 +0600

    Create manufacturing_heavy_free.ts

---
+++ src/super_vault/manufacturing_heavy_free.ts
@@ -0,0 +1,24 @@
+export const manufacturing_heavy_free = {
+  ingress_registry: {
+    sector_id: 7,
+    sector_key: "manufacturing_heavy_industry",
+    native_label: "Manufacturing & Heavy Industry",
+    tier_access_gate: "100_PERCENT_FREE_ORGANIC_MAGNET",
+  },
+  topical_authority_filters: {
+    minimum_intent_threshold: 20,
+    traffic_capture_strategy: "EXHAUSTIVE_FULL_SECTOR_SCAN",
+  },
+  autonomous_ai_directives: {
+    target_engine_velocity: "WORLD_CLASS_INFORMATIONAL_AUTHORITY",
+    word_count_boundary: { min: 1500, max: 3000 },
+    native_tone_delivery: "100% Human-Edited Essence, Zero-Robotic Fingerprint, Elite Intellectual",
+    shariah_intercept_tokens: [
+      "gambling",
+      "betting",
+      "unveiled_media",
+      "exploitative_labor_models",
+      "military_aggression_arms"
+    ]
+  }
+};

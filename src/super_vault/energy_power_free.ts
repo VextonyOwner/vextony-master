f5bb89dfbc584d390401315f97e6f20d298a81ed
MindSpark Universe <259244303+VextonyOwner@users.noreply.github.com>
2026-08-25 15:10:00 +0600

    Create energy_power_free.ts

---
+++ src/super_vault/energy_power_free.ts
@@ -0,0 +1,24 @@
+export const energy_power_free = {
+  ingress_registry: {
+    sector_id: 6,
+    sector_key: "energy_power_mining",
+    native_label: "Energy, Power, Utilities & Mining",
+    tier_access_gate: "100_PERCENT_FREE_ORGANIC_MAGNET",
+  },
+  topical_authority_filters: {
+    minimum_intent_threshold: 20,
+    traffic_capture_strategy: "EXHAUSTIVE_FULL_SECTOR_SCAN",
+  },
+  autonomous_ai_directives: {
+    target_engine_velocity: "WORLD_CLASS_INFORMATIONAL_AUTHORITY",
+    word_count_boundary: { min: 1500, max: 3000 },
+    native_tone_delivery: "100% Human-Edited Essence, Zero-Robotic Fingerprint, Elite Intellectual",
+    shariah_intercept_tokens: [
+      "gambling",
+      "betting",
+      "unveiled_media",
+      "monopoly_hoarding_tactics",
+      "usury_infrastructure_bonds"
+    ]
+  }
+};

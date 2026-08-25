73eae981e85da38b364a64b460fa21a55d8ae0b6
MindSpark Universe <259244303+VextonyOwner@users.noreply.github.com>
2026-08-25 14:55:04 +0600

    Create education_research_free.ts

---
+++ src/super_vault/education_research_free.ts
@@ -0,0 +1,24 @@
+export const education_research_free = {
+  ingress_registry: {
+    sector_id: 3,
+    sector_key: "education_training_pure_research",
+    native_label: "Education, Training & Pure Research",
+    tier_access_gate: "FREE_VIRAL_HOOK",
+  },
+  topical_authority_filters: {
+    minimum_intent_threshold: 25,
+    traffic_capture_strategy: "EXHAUSTIVE_FULL_SECTOR_SCAN",
+  },
+  autonomous_ai_directives: {
+    target_engine_velocity: "WORLD_CLASS_INFORMATIONAL_AUTHORITY",
+    word_count_boundary: { min: 1500, max: 3000 },
+    native_tone_delivery: "100% Human-Edited Essence, Zero-Robotic Fingerprint, Elite Intellectual",
+    shariah_intercept_tokens: [
+      "gambling",
+      "betting",
+      "unveiled_media",
+      "secular_indoctrination",
+      "riba_scholarships"
+    ]
+  }
+};
